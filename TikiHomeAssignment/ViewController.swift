e23cc25c7392068337b9eef3e11ee74de0cba347
hp1909 <hoangphuc1909@live.com>
2019-07-01 18:24:49 +0700

    Initial Commit

---
+++ TikiHomeAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TikiHomeAssignment
+//
+//  Created by Phuc Hoang on 7/1/19.
+//  Copyright © 2019 Phuc Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
